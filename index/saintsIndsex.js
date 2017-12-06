4425d338569f1be3e36d24ffe5534e9ea62f428d
Apple <edward.king86@hotmail.com>
2017-12-06 21:32:18 +0000

    changeschanged html on navbar links to pages to sort form ../ and edited READ.me file

--- index/saintsIndsex.js
+++ index/saintsIndsex.js
@@ -1,22 +1,6 @@
-function validateFilledOut() {
-    var x = document.forms["myForm"]["fName"].value;
-    if (x == "") {
+
+
+function validateForm() {
         alert("Name must be filled out");
         return false;
-    }
 }
-
-function validateEmail()
-      {
-         var emailID = document.myForm.email.value;
-         atpos = emailID.indexOf("@");
-         dotpos = emailID.lastIndexOf(".");
-         
-         if (atpos < 1 || ( dotpos - atpos < 2 )) 
-         {
-            alert("Please enter correct email ID")
-            document.myForm.email.focus() ;
-            return false;
-         }
-         return( true );
-      }
