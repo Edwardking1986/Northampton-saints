25d967f5a875b5af1908a5c0456ba2e9007caac8
Apple <edward.king86@hotmail.com>
2018-01-09 21:20:54 +0000

    changed script.js to change space to - in myInput

--- index/saintsIndsex.js
+++ index/saintsIndsex.js
@@ -1,5 +1,5 @@
 
-
 function validateFilledOut() {
-    alert("thank you for your request for the news letter!");
+	var name = document.getElementById("fname").value;
+    alert("thank you for your request " + name + " the news letter will be sent!");
 }
--- team page/script.js
+++ team page/script.js
@@ -1,6 +1,6 @@
  // this allows us to filter players profile
  function searchFilter () {
- 	var filter = document.getElementById("myInput").value.toUpperCase()
+ 	var filter = document.getElementById("myInput").value.toUpperCase().replace(" ", "-")
  	var cards = document.getElementsByClassName("card");
  	for (var i = 0; i<cards.length; i++ ) {
  		var card = cards[i];
